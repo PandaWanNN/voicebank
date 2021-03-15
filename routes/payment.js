e7d47d29f2ce5413789daff0e75c15515c9ae3c8
thomas <t.neff86@gmail.com>
2021-03-15 17:18:53 +0100

    enable authentification on payment

--- routes/payment.js
+++ routes/payment.js
@@ -1,3 +1,5 @@
+const auth = require('./authentification')
+
 const invoices = new Map();
 invoices.set(1, "Strom & Wasser AG, fällig am 2. Juni 2021, über CHF 421.75");
 invoices.set(2, "Kreditkarten AG, fällig am 2. Mai 2021, über CHF 1598.10");
@@ -6,15 +8,17 @@ invoices.set(4, "Handy AG, fällig am 15. Mai 2021, über CHF 35.50");
 
 module.exports = {
     payment: function payment(agent) {
-        const startMessage = "Sie haben 4 Rechnungen in Ihrem e-Bill Postfach:\n" +
-            "Die erste Rechnung ist von Handy AG, über CHF 35.50.\n" +
-            "Die zweite von Krankenkasse AG, über CHF 345.50.\n" +
-            "Die dritte von Kreditkarten AG, über CHF 1598.10.\n" +
-            "und die vierte von den Strom & Wasser AG, über CHF 421.75.\n" +
-            "\n" +
-            createPaymentQuestionPhrase();
+        auth.authenticate(agent, () => {
+            const startMessage = "Sie haben 4 Rechnungen in Ihrem e-Bill Postfach:\n" +
+                "Die erste Rechnung ist von Handy AG, über CHF 35.50.\n" +
+                "Die zweite von Krankenkasse AG, über CHF 345.50.\n" +
+                "Die dritte von Kreditkarten AG, über CHF 1598.10.\n" +
+                "und die vierte von den Strom & Wasser AG, über CHF 421.75.\n" +
+                "\n" +
+                createPaymentQuestionPhrase();
 
-        agent.add(startMessage);
+            agent.add(startMessage);
+        });
     },
 
     paymentYes: function paymentYes(agent) {
